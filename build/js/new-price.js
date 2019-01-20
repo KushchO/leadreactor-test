58c42dcc7d0b608872d43d66766b825415686fd0
KushchO <lvioleg@gmail.com>
2019-01-20 11:54:57 +0300

    Исправил ссылку

--- build/js/new-price.js
+++
@@ -1,5 +0,0 @@
-'use srtict';
-
-(function () {
-
-})();
---
+++ build/js/slider.js
@@ -0,0 +1,59 @@
+var slideNow = 1;
+var slideCount = $('.slider__list').children().length;
+var slideInterval = 3000;
+var navBtnId = 0;
+var translateWidth = 0;
+
+$(document).ready(function() {
+    var switchInterval = setInterval(nextSlide, slideInterval);
+
+    $('.slider').hover(function() {
+        clearInterval(switchInterval);
+    }, function() {
+        switchInterval = setInterval(nextSlide, slideInterval);
+    });
+
+    $('.slider__next').click(function() {
+        nextSlide();
+    });
+
+    $('.slider__previous').click(function() {
+        prevSlide();
+    });
+});
+
+
+function nextSlide() {
+    if (slideNow == slideCount || slideNow <= 0 || slideNow > slideCount) {
+        $('.slider__list').css('transform', 'translate(0, 0)');
+        slideNow = 1;
+    } else {
+        translateWidth = -$('.slider').width() * (slideNow);
+        $('.slider__list').css({
+            'transform': 'translate(' + translateWidth + 'px, 0)',
+            '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+            '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+        });
+        slideNow++;
+    }
+}
+
+function prevSlide() {
+    if (slideNow == 1 || slideNow <= 0 || slideNow > slideCount) {
+        translateWidth = -$('.slider').width() * (slideCount - 1);
+        $('.slider__list').css({
+            'transform': 'translate(' + translateWidth + 'px, 0)',
+            '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+            '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+        });
+        slideNow = slideCount;
+    } else {
+        translateWidth = -$('.slider').width() * (slideNow - 2);
+        $('.slider__list').css({
+            'transform': 'translate(' + translateWidth + 'px, 0)',
+            '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+            '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+        });
+        slideNow--;
+    }
+}
